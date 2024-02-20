945f5426aed505b7be08fbb2d7cb7dbf71cacf1e
Nicolas Denier <nicodenier@laposte.net>
2024-02-20 19:15:43 +0100

    project skyline on terrain

--- lidar/tools/camera.py
+++ lidar/tools/camera.py
@@ -1,4 +1,5 @@
 import numpy as np
+import open3d as o3d
 import open3d.visualization.gui as gui
 
 def get_focal_length_px(f, width, height, diag):
@@ -106,3 +107,36 @@ def callbacks(parameters, view_point):
     key_to_callback[ord("L")] = look_at_view_point
 
     return key_to_callback
+
+def save_skyline_with_terrain(pc, skyline_points, image_path):
+    '''Render an orthographic projection (no perspective) of the terrain from above, and add the skyline. Save it as image
+
+    Args:
+        pc (open3d.geometry.PointCloud): the terrain point cloud
+        skyline_points (np.array (360,3)): skyline cartesian coordinates
+        image_path (str): path and name where to save the image 
+    '''
+    # connect each point with the next one (except last with first)
+    line_indices = [[i, i + 1] for i in range(0, 360)]
+    line_indices[-1][1] = 0
+    # create open3d lines from skyline points
+    lines = o3d.geometry.LineSet()
+    lines.points = o3d.utility.Vector3dVector(skyline_points)
+    lines.lines = o3d.utility.Vector2iVector(line_indices)
+    #colors = [[1.0, 0.2, 0.7] for _ in range(0, 360)]
+    #lines.colors = o3d.utility.Vector3dVector(colors)
+
+    # Generate a render
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=500, height=500, visible=False)
+    vis.add_geometry(pc)
+    vis.add_geometry(lines)
+    # fov < 5 is rendered as orthographic projection
+    vis.get_view_control().change_field_of_view(-60)
+    vis.get_view_control().set_zoom(0.55)
+    vis.poll_events()
+    vis.update_renderer()
+    # save the image
+    vis.capture_screen_image(image_path)
+    print('Image saved at',image_path)
+    vis.destroy_window()
--- lidar/tools/project.py
+++ lidar/tools/project.py
@@ -18,16 +18,25 @@ def to_lambert93(lat, lon, altitude):
     point = np.array(projector.transform(lat,lon)+(altitude,))
     return point
 
-def array_to_spherical(pc, view_point):
-    def point_to_spherical(point):
+def array_cartesian_to_spherical(points, view_point):
+    '''Convert each cartesian points [x,y,z] of an array to spherical coordinates [r,theta,phi] (physics convention).
+
+    Args:
+        points (np.array (n,3)): list of points
+        view_point (list or np.array (1,3)): viewpoint to consider as origin
+
+    Return:
+        np.array: array of spherical coordinates
+    '''
+    def cartesian_to_spherical(point):
         '''Convert a point [x,y,z] to spherical coordinates [r,theta,phi] (physics convention).
         The origin is the view_point.
 
         Args:
-            point (np.array): [x,y,z]
+            point (list or np.array (1,3)): [x,y,z]
 
         Returns:
-            np.array: [r,theta,phi]
+            list: [r,theta,phi]
         '''
         x0, y0, z0 = view_point
         x, y, z = point
@@ -43,8 +52,28 @@ def array_to_spherical(pc, view_point):
         else: #x<x0
             phi = -np.arcsin(ydif/np.sqrt(rho2))+np.pi
         return [r, theta, phi]
-    spherical = np.apply_along_axis(point_to_spherical, 1, np.asarray(pc.points))
+    spherical = np.apply_along_axis(cartesian_to_spherical, 1, np.asarray(points))
     return spherical
+
+def spherical_to_cartesian(point, replaceZ=False):
+    '''Convert a point from spherical coordinates [r,theta,phi] to cartesian [x,y,z]
+
+    Args:
+        point (list or np.array (1,3)): the point in spherical coordinates (angles in radian)
+        getZ (bool or int, optional): whether or not to compute the z dimension. Defaults to False.
+
+    Returns:
+        list: [x,y] or [x,y,z]
+    '''
+    r, theta, phi = point
+    rho = r*np.cos(theta)
+    x = rho*np.cos(phi)
+    y = rho*np.sin(phi)
+    if replaceZ:
+        return [x,y,replaceZ]
+    else:
+        z = r*np.sin(theta)
+        return [x, y, z]
 
 def get_deg_angles(spherical, decimals=0):
     '''Get angles in degree from spherical coordinates. Rounds only phi.
@@ -78,22 +107,21 @@ def delete_max(array):
     new_array = np.delete(array, imax)
     return m, new_array
 
-def get_skyline(spherical, threshold=1e-4):
+def get_skyline(angles, threshold=1e-4):
     '''Extract skyline from spherical coordinates points
 
     Args:
-        spherical (np.array (3,n)): array of spherical coordinates [[r,theta,phi],...]
+        angles (np.array (3,n)): array of spherical coordinates angles in degree [[theta,phi],...]
         threshold (float, optional): outliers distants by more than this value are removed. Defaults to 1e-4.
 
     Returns:
         np.array (360,): skyline, i.e. max elevation angle for each azimuth angle
     '''
-    spherical = get_deg_angles(spherical)
     # sort 
-    spherical = spherical[spherical[:, 1].argsort()]
-    phi_values = np.unique(spherical[:, 1], return_index=True)
+    angles = angles[angles[:, 1].argsort()]
+    phi_values = np.unique(angles[:, 1], return_index=True)
     # group by phi 
-    grouped_by_phi = np.split(spherical[:,0], phi_values[1][1:])
+    grouped_by_phi = np.split(angles[:,0], phi_values[1][1:])
 
     # get max theta for each phi
     skyline = np.empty(shape=(360,))
@@ -106,11 +134,43 @@ def get_skyline(spherical, threshold=1e-4):
         skyline[i%360] = np.max(thetas)
     return skyline
 
-def plot_skyline(skyline):
+def plot_skyline(skyline, title):
+    '''Plot the maximum elevation angle according to the azimuth angle
+
+    Args:
+        skyline (np.array (360,)): the skyline data (max theta for each phi)
+        title (str): the plot title
+    '''
     plt.style.use('default')
     plt.figure(figsize=(20,5))
     plt.plot(skyline, linewidth=3, color='sienna')
     font_params = {'size':15}
+    plt.title(title, fontdict=font_params)
     plt.xlabel("Azimuth angle (°)", fontdict=font_params)
     plt.ylabel("Max elevation angle (°)", fontdict=font_params)
     plt.show()
+
+def skyline_to_cartesian(spherical, angles, skyline, view_point, max_z):
+    '''Transform the skyline to cartesian coordinates, so it can be diplayed with the terrain
+
+    Args:
+        spherical (np.array (n,3)): spherical coordinates of the terrain points
+        angles (np.array (n,2)): angles only in degree of the terrain points
+        skyline (np.array (360,)): max elevation angle for each azimuth
+        view_point (list or np.array (1,3)): viewpoint considered as origin for spherical coordinates
+        max_z (float): maximum altitude in the point cloud
+
+    Returns:
+        np.array (n,3): array of coordinates [x,y,z] for each point of the skyline
+    '''
+    skyline_points = np.empty(shape=(360,3))
+    # for each angle pair
+    for phi, theta in enumerate(skyline):
+        # get the index corresponding to that angle combination (spherical and angles have the same order)
+        i = np.argwhere((angles[:,0]==theta) & ((angles[:,1]+90)%360==phi))[0][0]
+        # convert the point to cartesian coordinates
+        cart_point = spherical_to_cartesian(spherical[i], replaceZ=max_z)
+        # translate cartesian conversion to viewpoint
+        skyline_points[phi] = np.add(cart_point, view_point)
+    return skyline_points
+
